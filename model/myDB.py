deef35cc6e9b71a8a1014465ab119185a550577b
John Mortensen <jmortensen@powayusd.com>
2021-05-31 06:17:11 -0700

    Integrated Mr H's work

--- model/myDB.py
+++ model/myDB.py
@@ -1,6 +1,8 @@
 """ database setup to support db examples """
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
+from flask_migrate import Migrate
+from flask_restful import Resource, Api
 
 from __init__ import app
 
@@ -10,35 +12,97 @@ app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 db = SQLAlchemy(app)
 
+Migrate(app, db)
+api = Api(app)
+url_prefix = "/model"
+
 
 # declare the users database model
 class Users(db.Model):
-    UserID = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=False, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), unique=False, nullable=False)
     phone = db.Column(db.String(255), unique=False, nullable=False)
+
+    def __init__(self, name, email, password, phone):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.phone = phone
+
+    def json(self):
+        return {
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+            "phone": self.phone
+        }
+
+    class Name(Resource):
+
+        def get(self, name):
+            person = Users.query.filter_by(name=name).first()
+
+            if person:
+                return person.json()
+
+            return {'name': None}, 404
+
+        def delete(self, name):
+            person = Users.query.filter_by(name=name).first()
+            db.session.delete(person)
+            db.session.commit()
+
+    class UpdateName(Resource):
+
+        def put(self, name, email):
+            person = Users.query.filter_by(email=email)
+            person.update(dict(name=name))
+            db.session.commit()
+
+    class CreateUser(Resource):
+
+        def post(self, name, email, password, phone):
+            person = Users(name=name, email=email, password=password, phone=phone)
+            db.session.add(person)
+            db.session.commit()
+
+            return person.json()
+
+    class Read(Resource):
+        def get(self):
+            people = Users.query.all()
+
+            return [peep.json() for peep in people]
+
+    api.add_resource(Name, url_prefix+'/name/<string:name>')
+    api.add_resource(CreateUser, url_prefix+'/create/<string:name>/<string:email>/<string:password>/<string:phone>')
+    api.add_resource(Read, url_prefix+'/read')
+    api.add_resource(UpdateName, url_prefix+'/update/<string:name>/<string:email>')
+
 
 
 # CRUD create/add a new record to the table
 # user_dict{} expects name, email, password, phone
 def model_create(user_dict):
     """prepare data for primary table extracting from form"""
-    user = Users(name=user_dict["name"],
-                 email=user_dict["email"],
-                 password=user_dict["password"],
-                 phone=user_dict["phone"])
-    """add and commit data to user table"""
-    db.session.add(user)
-    db.session.commit()
+    if Users.query.filter_by(email=user_dict['email']).first() is None:
+        user = Users(name=user_dict["name"],
+                     email=user_dict["email"],
+                     password=user_dict["password"],
+                     phone=user_dict["phone"])
+        """add and commit data to user table"""
+        db.session.add(user)
+        db.session.commit()
 
 
 # CRUD read: filter single record in table based off of userid
 # userid required
 def model_read(userid):
     """filter users by userid"""
-    user = Users.query.filter_by(UserID=userid).first()
-    user_dict = {'id': user.UserID,
+    user = Users.query.filter_by(userID=userid).first()
+    user_dict = {'id': user.userID,
                  'name': user.name,
                  'email': user.email,
                  'password': user.password,
@@ -51,8 +115,8 @@ def model_read(userid):
 def model_update(user_dict):
     """fetch userid"""
     userid = user_dict["userid"]
-    if Users.query.filter_by(UserID=userid).first() is not None:
-        db.session.query(Users).filter_by(UserID=userid).update(
+    if Users.query.filter_by(userID=userid).first() is not None:
+        db.session.query(Users).filter_by(userID=userid).update(
             {Users.phone: user_dict['phone']})
     """commit changes to database"""
     db.session.commit()
@@ -63,7 +127,7 @@ def model_update(user_dict):
 def model_delete(userid):
     """fetch userid"""
     userid = userid
-    db.session.query(Users).filter(Users.UserID == userid).delete()
+    db.session.query(Users).filter(Users.userID == userid).delete()
     """commit changes to database"""
     db.session.commit()
 
@@ -74,7 +138,7 @@ def model_query_all():
     records = []
     users = Users.query.all()
     for user in users:
-        user_dict = {'id': user.UserID, 'name': user.name, 'email': user.email, 'password': user.password,
+        user_dict = {'id': user.userID, 'name': user.name, 'email': user.email, 'password': user.password,
                      'phone': user.phone}
         records.append(user_dict)
     return records
@@ -86,7 +150,7 @@ def model_query_emails():
     users = []
     records = Users.query.all()
     for record in records:
-        user_dict = {'id': record.UserID, 'email': record.email}
+        user_dict = {'id': record.userID, 'email': record.email}
         users.append(user_dict)
     return users
 
@@ -97,7 +161,7 @@ def model_query_phones():
     users = []
     records = Users.query.all()
     for record in records:
-        user_dict = {'id': record.UserID, 'phone': record.phone}
+        user_dict = {'id': record.userID, 'phone': record.phone}
         users.append(user_dict)
     return users
 
@@ -105,13 +169,13 @@ def model_query_phones():
 if __name__ == "__main__":
     """Tester data for table"""
     db.create_all()
-    u1 = Users(name='Thomas Edison', email='tedison@example.com', password='toby', phone="111-111-1111")
-    u2 = Users(name='Nicholas Tesla', email='ntesla@example.com', password='niko', phone="111-111-2222")
-    u3 = Users(name='Alexander Graham Bell', email='agbell@example.com', password='lex', phone="111-111-3333")
-    u4 = Users(name='Eli Whitney', email='eliw@example.com', password='whit', phone="111-111-4444")
-    u5 = Users(name='John Mortensen', email='jmort1021@gmail.com', password='123qwerty', phone="858-775-4956")
-    u6 = Users(name='John Mortensen', email='jmort1021@yahoo.com', password='123qwerty', phone="858-775-4956")
-    u7 = Users(name='John Mortensen', email='jmort1021@yahoo.com', password='123qwerty', phone="858-679-1294")
+    u1 = Users(name='Thomas Edison', email='tedison@example.com', password='toby', phone="1111111111")
+    u2 = Users(name='Nicholas Tesla', email='ntesla@example.com', password='niko', phone="1111112222")
+    u3 = Users(name='Alexander Graham Bell', email='agbell@example.com', password='lex', phone="1111113333")
+    u4 = Users(name='Eli Whitney', email='eliw@example.com', password='whit', phone="1111114444")
+    u5 = Users(name='John Mortensen', email='jmort1021@gmail.com', password='123qwerty', phone="8587754956")
+    u6 = Users(name='John Mortensen', email='jmort1021@yahoo.com', password='123qwerty', phone="8587754956")
+    u7 = Users(name='John Mortensen', email='jmort1021@yahoo.com', password='123qwerty', phone="8586791294")
     table = [u1, u2, u3, u4, u5, u6, u7]
     for row in table:
         try:
@@ -119,7 +183,7 @@ if __name__ == "__main__":
             db.session.commit()
         except IntegrityError:
             db.session.remove()
-            print(f"Records exist or duplicate email: {row.email}")
+            print(f"Records exist, duplicate email, or error: {row.email}")
 
     print("Table: Users")
     result = model_query_all()
