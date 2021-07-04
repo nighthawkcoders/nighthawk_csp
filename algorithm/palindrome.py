abbd9536bc08c99ad0b0228b1acbe1dac6b3abca
John Mortensen <jmortensen@powayusd.com>
2021-07-04 16:50:18 -0700

    Add some logging and evaluation to palindrome

--- algorithm/palindrome.py
+++ algorithm/palindrome.py
@@ -2,27 +2,41 @@ import re
 
 
 class Palindrome:
+    # palindrome initializer method
     def __init__(self, candidate):
-        self._candidate = candidate
-        self._is_a_palindrome = False
-        self._tests = 0
+        # input values
+        self._candidate = candidate  # input string
+        self._length = len(candidate)  # input length
+        # analysis values
+        self._is_a_palindrome = False  # initialize status
+        self._az09 = re.sub(r'[^a-zA-Z0-9]', '', self._candidate)  # alpha numeric characters
+        self._analysis = []  # array of tests
+        self._tests = 0  # counter of tests performed
+        # evaluate for palindrome
         self.is_palindrome()
 
+    # palindrome tester method
     def is_palindrome(self):
-        # remove all non alphanumeric using regular expression
-        c = re.sub(r'[^a-zA-Z0-9]', '', self._candidate)
+        c = self._az09
         # Run loop from 0 to len/2 of string (middle is exit point)
         tests = int(len(c) / 2)
         for i in range(0, tests):
             front = c[i];
             back = c[len(c) - i - 1]
             if front.lower() == back.lower():
+                self.logger(front, back, True)
                 self._tests += 1
             else:
+                self.logger(front, back, False)
                 return
         self._is_a_palindrome = True
         return
 
+    # palindrome logging
+    def logger(self, front, back, result):
+        self._analysis.append({"test": self._tests, "front": front, "back": back, "result": result})
+
+    # getters follow
     @property
     def candidate(self):
         return self._candidate
@@ -34,3 +48,7 @@ class Palindrome:
     @property
     def isPalindrome(self):
         return self._is_a_palindrome
+
+    @property
+    def analysis(self):
+        return self._analysis
