e59e11aa9386172bf643e760b9f0b4f4d3399e98
John Mortensen <jmortensen@powayusd.com>
2021-08-06 20:35:39 -0700

    RGB using data and jinja2

--- starter/starter.py
+++ starter/starter.py
@@ -6,12 +6,18 @@ starter_bp = Blueprint('starter', __name__,
                        static_folder='static',
                        static_url_path='assets')
 
-
-@starter_bp.route('/rgb', methods=["GET", "POST"])
-def rgb():
-    return render_template('starter/rgb.html')
-
-
 @starter_bp.route('/binary/')
 def binary():
     return render_template("starter/binary.html")
+
+
+@starter_bp.route('/rgb', methods=["GET", "POST"])
+def rgb():
+    colors = [
+        {'label': "black", 'file': "black-square.png"},
+        {'label': "red", 'file': "red-square.png"},
+        {'label': "green", 'file': "green-square.png"},
+        {'label': "blue", 'file': "blue-square.png"},
+        {'label': "white", 'file': "white-square.png"}
+    ]
+    return render_template('starter/rgb.html', colors=colors)
