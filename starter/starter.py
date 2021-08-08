e8332f38aa4736a1e45abe4ec1efe14e46ebe588
John Mortensen <jmortensen@powayusd.com>
2021-08-07 18:24:08 -0700

    test case for large file in local main of starter

--- starter/starter.py
+++ starter/starter.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from PIL import Image
+from PIL import Image, ImageDraw
 import numpy
 
 starter_bp = Blueprint('starter', __name__,
@@ -19,21 +19,21 @@ def rgb():
     return render_template('starter/rgb.html', colors=color_data())
 
 
-def color_data(path="starter/static/"):  # path of blueprint run is default
+def color_data(path="starter/static/", color_dict=None):  # path of blueprint run is default
     # prefill with label and file
-    color_dict = [
-        {'source': "Peter Carolin", 'label': "Lassen Volcano", 'file': "lassen-volcano-256.jpg"},
-        {'source': "iconsdb.com", 'label': "Black square", 'file': "black-square-16.png"},
-        {'source': "iconsdb.com", 'label': "Red square", 'file': "red-square-16.png"},
-        {'source': "iconsdb.com", 'label': "Green square", 'file': "green-square-16.png"},
-        {'source': "iconsdb.com", 'label': "Blue square", 'file': "blue-square-16.png"},
-        {'source': "iconsdb.com", 'label': "White square", 'file': "white-square-16.png"},
-        {'source': "iconsdb.com", 'label': "Blue square", 'file': "blue-square-16.jpg"}
-    ]
+    if color_dict is None:
+        color_dict = [
+            {'source': "Peter Carolin", 'label': "Lassen Volcano", 'file': "lassen-volcano-256.jpg"},
+            {'source': "iconsdb.com", 'label': "Black square", 'file': "black-square-16.png"},
+            {'source': "iconsdb.com", 'label': "Red square", 'file': "red-square-16.png"},
+            {'source': "iconsdb.com", 'label': "Green square", 'file': "green-square-16.png"},
+            {'source': "iconsdb.com", 'label': "Blue square", 'file': "blue-square-16.png"},
+            {'source': "iconsdb.com", 'label': "White square", 'file': "white-square-16.png"},
+            {'source': "iconsdb.com", 'label': "Blue square", 'file': "blue-square-16.jpg"}
+        ]
     # calculate attributes of image
     for color in color_dict:
         file = path + color['file']
-        print(file)
         image_reference = Image.open(file)
         image_data = image_reference.getdata()
         color['format'] = image_reference.format
@@ -46,13 +46,18 @@ def color_data(path="starter/static/"):  # path of blueprint run is default
             hex_value = hex(code[0])[-2:] + hex(code[1])[-2:] + hex(code[2])[-2:]
             hex_value = hex_value.replace("x", "0")
             color['hex_array'].append("#" + hex_value)
-            bin_value = bin(int('1'+hex_value, 16))[3:]
+            bin_value = bin(int('1' + hex_value, 16))[3:]
             color['binary_array'].append("0b" + bin_value)
     return color_dict
 
 
 if __name__ == "__main__":
-    colors = color_data("static/")  # path of local run
+    local_path = "static/"
+    color_test = [
+        {'source': "Peter Carolin", 'label': "Lassen Volcano", 'file': "lassen-volcano-256.jpg"},
+        {'source': "Peter Carolin", 'label': "Lassen Volcano", 'file': "lassen-volcano-original.jpg"},
+    ]
+    colors = color_data(local_path, color_test)  # path of local run
     for row in colors:
         # summarize some details about the image
         print(row['label'])
@@ -62,4 +67,9 @@ if __name__ == "__main__":
         print(row['data'])
         print(row['hex_array'])
         print(row['binary_array'])
+        filename = local_path + row['file']
+        image_ref = Image.open(filename)
+        draw = ImageDraw.Draw(image_ref)
+        draw.text((0, 0), "Size is {0} X {1}".format(*row['size']))
+        image_ref.show()
 print()
