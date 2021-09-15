84bb572632827995dc7d69ff142336e43a820c0f
John Mortensen <jmortensen@powayusd.com>
2021-09-15 06:22:15 -0700

    image data with comments, formatting on truncated items in HTML

--- algorithm/image.py
+++ algorithm/image.py
@@ -4,22 +4,22 @@ import base64
 from io import BytesIO
 
 
-# image to base64 conversion, learn about base64 on wikipedia https://en.wikipedia.org/wiki/Base64
+# image (PNG, JPG) to base64 conversion (string), learn about base64 on wikipedia https://en.wikipedia.org/wiki/Base64
 def image_base64(img, img_type):
     with BytesIO() as buffer:
         img.save(buffer, img_type)
         return base64.b64encode(buffer.getvalue()).decode()
 
 
-# formatter preps string format for <img src=[this return value] ... />
+# formatter preps base64 string for inclusion, ie <img src=[this return value] ... />
 def image_formatter(img, img_type):
     return "data:image/" + img_type + ";base64," + image_base64(img, img_type)
 
 
 # color_data prepares a series of images for data analysis
-def image_data(path="static/img/", color_dict=None):  # path of static images is defaulted
-    if color_dict is None:  # color_dict is defined with defaults
-        color_dict = [
+def image_data(path="static/img/", img_list=None):  # path of static images is defaulted
+    if img_list is None:  # color_dict is defined with defaults
+        img_list = [
             {'source': "Peter Carolin", 'label': "Lassen Volcano", 'file': "lassen-volcano-256.jpg"},
             {'source': "iconsdb.com", 'label': "Black square", 'file': "black-square-16.png"},
             {'source': "iconsdb.com", 'label': "Red square", 'file': "red-square-16.png"},
@@ -29,41 +29,41 @@ def image_data(path="static/img/", color_dict=None):  # path of static images is
             {'source': "iconsdb.com", 'label': "Blue square", 'file': "blue-square-16.jpg"}
         ]
     # gather analysis data and meta data for each image, adding attributes to each row in table
-    for color in color_dict:
-        color['path'] = '/' + path  # path for HTML access (frontend)
-        file = path + color['file']  # file with path for local access (backend)
+    for img_dict in img_list:
+        img_dict['path'] = '/' + path  # path for HTML access (frontend)
+        file = path + img_dict['file']  # file with path for local access (backend)
         # Python Image Library operations
         img_reference = Image.open(file)
-        img_data = img_reference.getdata()
-        color['format'] = img_reference.format
-        color['mode'] = img_reference.mode
-        color['size'] = img_reference.size
+        img_data = img_reference.getdata()  # Reference https://www.geeksforgeeks.org/python-pil-image-getdata/
+        img_dict['format'] = img_reference.format
+        img_dict['mode'] = img_reference.mode
+        img_dict['size'] = img_reference.size
         # Conversion of original Image to Base64, a string format that serves HTML nicely
-        color['base64'] = image_formatter(img_reference, color['format'])
+        img_dict['base64'] = image_formatter(img_reference, img_dict['format'])
         # Numpy is used to allow easy access to data of image, python list
-        color['data'] = numpy.array(img_data)
-        color['hex_array'] = []
-        color['binary_array'] = []
-        # Data list of RGB data traversed and hex and binary lists are created and formated
-        for code in color['data']:
+        img_dict['data'] = numpy.array(img_data)
+        img_dict['hex_array'] = []
+        img_dict['binary_array'] = []
+        # 'data' is a list of RGB data, the list is traversed and hex and binary lists are calculated and formatted
+        for pixel in img_dict['data']:
             # hexadecimal conversions
-            hex_value = hex(code[0])[-2:] + hex(code[1])[-2:] + hex(code[2])[-2:]
+            hex_value = hex(pixel[0])[-2:] + hex(pixel[1])[-2:] + hex(pixel[2])[-2:]
             hex_value = hex_value.replace("x", "0")
-            color['hex_array'].append("#" + hex_value)
+            img_dict['hex_array'].append("#" + hex_value)
             # binary conversions
-            bin_value = bin(code[0])[2:].zfill(8) + " " + bin(code[1])[2:].zfill(8) + " " + bin(code[2])[2:].zfill(8)
-            color['binary_array'].append(bin_value)
-    return color_dict  # dictionary is returned with all the attributes of these images
+            bin_value = bin(pixel[0])[2:].zfill(8) + " " + bin(pixel[1])[2:].zfill(8) + " " + bin(pixel[2])[2:].zfill(8)
+            img_dict['binary_array'].append(bin_value)
+    return img_list  # list is returned with all the attributes for each image dictionary
 
 
 # run this as standalone tester to see data printed in terminal
 if __name__ == "__main__":
     local_path = "../static/img/"
-    color_test = [
+    img_test = [
         {'source': "iconsdb.com", 'label': "Red square", 'file': "red-square-16.png"},
     ]
-    colors = image_data(local_path, color_test)  # path of local run
-    for row in colors:
+    items = image_data(local_path, img_test)  # path of local run
+    for row in items:
         # print some details about the image so you can validate that it looks like it is working
         print(row['label'])
         print(row['format'])
