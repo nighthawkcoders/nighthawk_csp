23d9a3769cba6ee6e4cd6aa0cb518e830cd18d14
John Mortensen <jmortensen@powayusd.com>
2021-10-08 19:08:49 -0700

    add my own joke api

--- restapi/restapi.py
+++ restapi/restapi.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 import requests
 
 restapi_bp = Blueprint('restapi', __name__,
@@ -6,22 +6,52 @@ restapi_bp = Blueprint('restapi', __name__,
                        template_folder='templates',
                        static_folder='static', static_url_path='assets')
 
+jokes = []
+joke_list = [
+    "If you give someone a program... you will frustrate them for a day; if you teach them how to program... you will "
+    "frustrate them for a lifetime.",
+    "Q: Why did I divide sin by tan? A: Just cos.",
+    "UNIX is basically a simple operating system... but you have to be a genius to understand the simplicity.",
+    "Enter any 11-digit prime number to continue.",
+    "If at first you don,t succeed; call it version 1.0.",
+    "Java programmers are some of the most materialistic people I know, very object-oriented",
+    "The oldest computer can be traced back to Adam and Eve. It was an apple but with extremely limited memory. Just "
+    "1 byte. And then everything crashed.",
+    "Q: Why did Wi-Fi and the computer get married? A: Because they had a connection",
+    "Bill Gates teaches a kindergarten class to count to ten. “1, 2, 3, 3.1, 95, 98, ME, 2000, XP, Vista, 7, 8, 10.",
+    "Q: What’s a aliens favorite computer key? A: the space bar!",
+    "There are 10 types of people in the world: those who understand binary, and those who don’t.",
+    "If it wasn't for C, we’d all be programming in BASI and OBOL.",
+    "Computers make very fast, very accurate mistakes.",
+    "Q: Why is it that programmers always confuse Halloween with Christmas? A: Because 31 OCT = 25 DEC.",
+    "Q: How many programmers does it take to change a light bulb? A: None. It’s a hardware problem.",
+    "The programmer got stuck in the shower because the instructions on the shampoo bottle said: Lather, Rinse, Repeat.",
+    "Q: What is the biggest lie in the entire universe? A: I have read and agree to the Terms and Conditions.",
+    'An SQL statement walks into a bar and sees two tables. It approaches, and asks may I join you?'
+]
 
-@restapi_bp.route('/joke', methods=['GET', 'POST'])
-def joke():
-    # call to random joke web api
-    url = 'https://official-joke-api.appspot.com/jokes/programming/random'
-    response = requests.get(url)
-    # formatting variables from return
-    setup = response.json()[0]['setup']
-    punchline = response.json()[0]['punchline']
-    return render_template("restapi/joke.html", setup=setup, punchline=punchline)
+
+def _find_next_id():
+    return max(jokes["id"] for joke in jokes) + 1
+
+
+def _init_jokes():
+    id = 1
+    for joke in joke_list:
+        jokes.append({"id": id, "joke": joke})
+        id += 1
+
+
+@restapi_bp.route('/jokes')
+def get_jokes():
+    if len(jokes) == 0:
+        _init_jokes()
+    return jsonify(jokes)
 
 
 @restapi_bp.route('/covid19', methods=['GET', 'POST'])
 def covid19():
     url = "https://corona-virus-world-and-india-data.p.rapidapi.com/api"
-
     headers = {
         'x-rapidapi-key': "dec069b877msh0d9d0827664078cp1a18fajsn2afac35ae063",
         'x-rapidapi-host': "corona-virus-world-and-india-data.p.rapidapi.com"
