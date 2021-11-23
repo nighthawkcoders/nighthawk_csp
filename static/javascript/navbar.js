e81c59b7afa99e514895ba5f7e12a66c4ac46f70
John Mortensen <jmortensen@powayusd.com>
2021-11-23 00:33:28 -0800

    Bootstrap preserving Dark Mode.

--- static/javascript/navbar.js
+++ static/javascript/navbar.js
@@ -1,8 +1,42 @@
 // Dark Mode Toggle
+
+const darkSwitch=document.getElementById("darkSwitch");
+window.addEventListener("load",(function(){
+    if(darkSwitch){
+        initDarkSwitch();
+        darkSwitch.addEventListener("change",(function(){
+            resetDarkSwitch();
+        }))
+    }
+}));
+
+function initDarkSwitch(){
+    const darkSwitchSelected=localStorage.getItem("darkSwitch") !==null
+        && localStorage.getItem("darkSwitch") === "dark";
+    darkSwitch.checked=darkSwitchSelected;
+    darkSwitchSelected
+        ? document.body.setAttribute("data-theme","dark")
+        : document.body.removeAttribute("data-theme")
+    if(darkSwitchSelected){
+        darkFunction();
+    }
+}
+
+function resetDarkSwitch(){
+    if(darkSwitch.checked){
+        document.body.setAttribute("data-theme","dark");
+        localStorage.setItem("darkSwitch","dark")
+    }else{
+        document.body.removeAttribute("data-theme");
+        localStorage.removeItem("darkSwitch")
+    }
+}
+
 function darkFunction() {
     let element = document.body;
     element.classList.toggle("dark-mode");
 }
+
 // Clock controller
 let clockElement = document.getElementById('clock');
 function clock() {
