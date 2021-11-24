76caae7d6bffb9f5321393b93b28be0e0d31a453
John Mortensen <jmortensen@powayusd.com>
2021-11-24 08:42:44 -0800

    Dark Mode JS updated for student review

--- static/javascript/navbar.js
+++ static/javascript/navbar.js
@@ -1,35 +1,39 @@
-// Dark Mode Toggle
-
+// Dark Mode Control
+// darkSwitch global element is set for convenience
 const darkSwitch=document.getElementById("darkSwitch");
-if(darkSwitch)
-    initDarkSwitch();
-
-function initDarkSwitch(){
-    const darkSwitchSelected=localStorage.getItem("darkSwitch") !==null
-        && localStorage.getItem("darkSwitch") === "dark";
+// establishes the initial state of darkSwitch on page load
+if(darkSwitch) {
+    // obtains the value of darkSwitch from localStorage
+    const darkSwitchSelected =
+        localStorage.getItem("darkSwitch") !== null &&
+        localStorage.getItem("darkSwitch") === "dark";
+    // sets the value of darkSwitch.checked to the global darkSwitch
     darkSwitch.checked=darkSwitchSelected;
-    darkSwitchSelected
-        ? document.body.setAttribute("data-theme","dark")
-        : document.body.removeAttribute("data-theme")
-    if(darkSwitchSelected){
-        darkFunction();
-    }
+    // if dark-mode, toggle the switch to dark-mode position
+    if (darkSwitchSelected)
+        document.body.classList.toggle("dark-mode");
 }
-
+// executes on each "dark mode" UI toggle
 function darkFunction() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-
+    // toggles between off (standard CSS) and dark-mode (.dark-mode CSS)
+    document.body.classList.toggle("dark-mode");
+    // localStorage is used to manage darkSwitch status
     darkSwitch.checked
         ? localStorage.setItem("darkSwitch","dark")
         : localStorage.removeItem("darkSwitch");
 }
+// End Dark Mode Control
 
-// Clock controller
+
+// Clock Control
+// clockElement global element set for convenience
 let clockElement = document.getElementById('clock');
+// Clock update executes on each interval set by timer
 function clock() {
     let date = new Date();
     clockElement.textContent = date.toLocaleString();
 }
-// Clock refresh frequency
+// Clock refresh timer, a reoccurring event
 setInterval(clock, 1000);
+// End Clock Control
+
