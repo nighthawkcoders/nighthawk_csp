40f82ce9622bd49ac183d16f2ee7997e73037ed3
John Mortensen <jmortensen@powayusd.com>
2022-02-16 19:10:21 -0800

    matrix adds

--- hacks/matrix.py
+++ hacks/matrix.py
@@ -25,22 +25,32 @@ def print_matrix3(matrix):
 
 # tester section
 if __name__ == "__main__":
-    print("3x3")
     matrix = [[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]]
 
+    print(len(matrix), "x 3")
     print_matrix1(matrix)
     print_matrix2(matrix)
     print_matrix3(matrix)
 
     print()
-    print("4x4")
     matrix = [[0, 1, 2, 3],
               [4, 5, 6, 7],
               [8, 9, "A", "B"],
               ["C", "D", "E", "F"]]
 
+    print(len(matrix), "x 4")
+    print_matrix1(matrix)
+    print_matrix2(matrix)
+    print_matrix3(matrix)
+
+    print()
+    matrix = [[0, 1],
+              [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+              [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]]
+
+    print(len(matrix), "x Mixed")
     print_matrix1(matrix)
     print_matrix2(matrix)
     print_matrix3(matrix)
