e9ef8f1b3eb5d6a4d96e6850a8b63bb191dc7435
suneelanaidu <88572244+suneelanaidu@users.noreply.github.com>
2022-03-04 18:02:47 -0800

    Update menuy.py
    Added submenu for patterns

--- hacks/menuy.py
+++ hacks/menuy.py
@@ -1,10 +1,18 @@
 # menuy.py - function style menu
 # Imports typically listed at top
 # each import enables us to use logic that has been abstracted to other files and folders
+import patterns
 import stringy
 import listy
 import loopy
-import mathy
+import mathpy
+from mody import advy
+# abstracted files in a folder (aka module)
+from mody import questy
+from wipy import funcy
+from wipy import prefuncy
+import patterns
+
 
 # Main list of [Prompts, Actions]
 # Two styles are supported to execute abstracted logic
@@ -25,6 +33,11 @@ sub_menu = [
     ["Primes", mathpy.primes],
 ]
 
+patterns_sub_menu = [
+    ["Patterns", "patterns1.py"],
+    ["PreFuncy", "wipy/prefuncy.py"],
+    ["Funcy", funcy.ship],
+]
 
 # Menu banner is typically defined by menu owner
 border = "=" * 25
@@ -34,31 +47,39 @@ banner = f"\n{border}\nPlease Select An Option\n{border}"
 def menuc():
     title = "Class Menu" + banner
     menu_list = main_menu.copy()
-    # for 4 -> Math
     menu_list.append(["Math", submenuc])
+    menu_list.append(["Patterns", patterns_submenuc])
     m = questy.Menu(title, menu_list)
     m.menu()  # method and data reside in object
 
 
 # def submenuc
-# using main_menu list:
+# using sub_menu list:
 # submenuc works similarly to menuc
 def submenuc():
     title = "Class Submenu" + banner
     m = questy.Menu(title, sub_menu)
     m.menu()
 
+# def patterns_submenuc
+# using patterns_sub_menu list:
+# patterns_submenuc works similarly to menuc
+def patterns_submenuc():
+    title = "Class Submenu" + banner
+    m = questy.Menu(title, patterns_sub_menu)
+    m.menu()
+
 
 # def menu
 # using main_menu list:
 # 1. main menu and submenu reference are created [Prompts, Actions]
-# 2. menu_list is sent as parameter to buildMenu function that has logic for menu control
+# 2. menu_list is sent as parameter to menuy.menu function that has logic for menu control
 def menu():
     title = "Function Menu" + banner
     menu_list = main_menu.copy()
     menu_list.append(["Math", submenu])
+    menu_list.append(["Patterns", patterns_submenu])
     buildMenu(title, menu_list)
-
 
 # def submenu
 # using sub menu list above:
@@ -66,7 +87,9 @@ def menu():
 def submenu():
     title = "Function Submenu" + banner
     buildMenu(title, sub_menu)
-
+def patterns_submenu():
+    title = "Function Submenu" + banner
+    buildMenu(title, patterns_sub_menu)
 
 def buildMenu(banner, options):
     # header for menu
