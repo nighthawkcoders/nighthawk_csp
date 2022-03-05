6e54a3719f061bd91b6113752bbfb7278f95ec18
John Mortensen <jmortensen@powayusd.com>
2022-03-05 14:19:53 -0800

    car listy added for Tech Talk

--- hacks/carlisty.py
+++ hacks/carlisty.py
@@ -19,17 +19,21 @@ InfoDb.append({
 })
 
 
+# reuse print style for all three loops
 def print_data(n):
-    print(InfoDb[n]["FirstName"], InfoDb[n]["LastName"])    # using comma puts space between values
-    print("\t", "Cars: ", end="")               # \t is a tab indent, end="" make sure no return occurs
-    print(", ".join(InfoDb[n]["Owns_Cars"]))    # join allows printing a string list with separator
+    print(InfoDb[n]["FirstName"], InfoDb[n]["LastName"])  # using comma puts space between values
+    print("\t", "Cars: ", end="")  # \t is a tab indent, end="" make sure no return occurs
+    print(", ".join(InfoDb[n]["Owns_Cars"]))  # join allows printing a string list with separator
+    print()
 
 
+# for loop iterates on length of InfoDb
 def for_loop():
     for n in range(len(InfoDb)):
         print_data(n)
 
 
+# while loop contains an initial n and an index incrementing statement (n += 1)
 def while_loop(n):
     while n < len(InfoDb):
         print_data(n)
@@ -37,6 +41,7 @@ def while_loop(n):
     return
 
 
+# recursion simulates loop incrementing on each call (n + 1) until exit condition is met
 def recursive_loop(n):
     if n < len(InfoDb):
         print_data(n)
@@ -44,10 +49,11 @@ def recursive_loop(n):
     return
 
 
+# this is test driver or code that plays when executed directly, versus import which will not run these statements
 if __name__ == "__main__":
     print("For loop")
     for_loop()
     print("While loop")
-    while_loop(0)
+    while_loop(0)  # requires initial index to start while
     print("Recursive loop")
-    recursive_loop(0)
+    recursive_loop(0)  # requires initial index to start recursion
