dbd3bb228a94f70b3f7abecc4c56d790d49d6a94
suneelanaidu <88572244+suneelanaidu@users.noreply.github.com>
2022-03-13 10:23:42 -0700

    Update fibonacci.py

--- hacks/fibonacci.py
+++ hacks/fibonacci.py
@@ -5,9 +5,14 @@ def fibonacci_of(n):
     return fibonacci_of(n - 1) + fibonacci_of(n - 2)  # Recursive case
 
 def driver():
-    n = int(input("Enter number of terms for the Fibonacci Sequence: "))
-    print("Fibonacci sequence of {0} terms is: ".format(n))
-    print([fibonacci_of(n) for n in range(15)])
+    n = input("Enter number of terms for the fibonacci sequence: ")
+    try:
+        n = int(n)
+        print("Fibonacci sequence of {0} terms is: ".format(n))
+        print([fibonacci_of(n) for n in range(n)])
+    except ValueError:
+        # not a number error
+        print(f"Not a number: {n}")
 
 if __name__ == "__main__":
     driver()
