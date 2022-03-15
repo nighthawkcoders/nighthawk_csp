85da5c7ec9694cfead219432410c4eff9ca3c9ff
suneelanaidu <88572244+suneelanaidu@users.noreply.github.com>
2022-03-14 22:21:28 -0700

    Create advy.py

---
+++ hacks/advy.py
@@ -0,0 +1,15 @@
+# advy.py - adventure starter
+
+def beach():
+    #Your beach function/animation here
+    print("beach function")
+
+
+def mountain():
+    #Your mountain function/animation here
+    print("mountain function")
+
+
+def lake():
+    #Your lake function/animation here
+    print("lake function")
