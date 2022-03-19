f8d67808c0d7269772dd7d4556646be878d6bae4
suneelanaidu <88572244+suneelanaidu@users.noreply.github.com>
2022-03-19 12:00:31 -0700

    Update fibonacci_class.py

--- hacks/fibonacci_class.py
+++ hacks/fibonacci_class.py
@@ -19,22 +19,24 @@ class Fibonacci:
 
 
 def tester():
-    #make a fibonacci object
-    fibo_of = Fibonacci()
-    n = input("Enter the number of terms: ")
-    try:
-        n = int(n)
-        # Validate the value of n
-        #The isinstance() function in Python returns true or false if a variable matches a
-        # specified data type. isinstance(variable_to_check, data_type)
-        if not (isinstance(n, int) and n >= 0):
-            raise ValueError
-        print("{0}th term  of Fibonacci sequence is: ".format(n))
-        print(fibo_of(n-1)) # print the nth term
-        print("Fibonacci sequence of {0} terms is: ".format(n))
-        print([fibo_of(i) for i in range(0,n)])
-    except:
-        print(f'Positive integer number expected, got "{n}"')
+    # Make a fibonacci object
+    while True:
+        fibo_of = Fibonacci()
+        n = input("Enter the number of terms: ")
+        try:
+            n = int(n)
+            # Validate the value of n
+            #The isinstance() function in Python returns true or false if a variable matches a
+            # specified data type. isinstance(variable_to_check, data_type)
+            if not (isinstance(n, int) and n >= 0):
+                raise ValueError
+            print("{0}th term  of Fibonacci sequence is: ".format(n))
+            print(fibo_of(n-1)) # print the nth term
+            print("Fibonacci sequence of {0} terms is: ".format(n))
+            print([fibo_of(i) for i in range(0,n)])
+            break
+        except:
+            print(f'Positive integer number expected, got "{n}" Try again.')
 
 if __name__ == "__main__":
     tester()
