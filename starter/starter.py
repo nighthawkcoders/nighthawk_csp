fb007e0432c9c7314267c20bf937b4c0cbb33390
John Mortensen <jmortensen@powayusd.com>
2022-03-30 13:54:49 -0700

    put back in place

--- starter/starter.py
+++ starter/starter.py
@@ -3,7 +3,8 @@ from flask import Blueprint, render_template, request
 from algorithm.image import image_data
 
 from pathlib import \
-    Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
+    Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and
+# -linux-11a072b58d5f
 
 app_starter = Blueprint('starter', __name__,
                         url_prefix='/starter',
