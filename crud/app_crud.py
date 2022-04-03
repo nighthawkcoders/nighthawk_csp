7cf1b4b4fe02397867177607b7f5984dd321bc00
John Mortensen <jmortensen@powayusd.com>
2022-04-03 06:40:47 -0700

    organize login information together

--- crud/app_crud.py
+++ crud/app_crud.py
@@ -11,28 +11,36 @@ app_crud = Blueprint('crud', __name__,
                      static_folder='static',
                      static_url_path='static')
 
-""" Application control for CRUD is main focus of this File, key features:
-    1.) User table queries
-    2.) app routes for CRUD (Blueprint)
+""" Blueprint is established to isolate Application control code for CRUD operations, key features:
+    1.) 'Users' table control methods, controls relationship between User Actions and Database Model
+    2.) Control methods are achieved using app routes for each CRUD operations
+    3.) login required to restrict CRUD operations to identified users
 """
 
 
-# Default URL
+# Default URL for Blueprint
 @app_crud.route('/')
-@login_required  # this is the code that Flask-Login uses to stop non logged in users
+@login_required  # Flask-Login uses this decorator to restrict acess to logged in users
 def crud():
     """obtains all Users from table and loads Admin Form"""
     return render_template("crud.html", table=users_all())
 
 
+# Flask-Login directs unauthorised users to this unauthorized_handler
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    return redirect(url_for('crud.crud_login'))
+
+
 # if login url, show phones table only
 @app_crud.route('/login/', methods=["GET", "POST"])
 def crud_login():
+    # obtains form inputs and fulfills login requirements
     if request.form:
-        # validation should be in HTML
         email = request.form.get("email"),
         password = request.form.get("password")
-        if login(email[0], password):
+        if login(email[0], password):       # zero index [0] used as email is a tuple
             return redirect(url_for('crud.crud'))
 
     # if not logged in, show the login page
@@ -41,16 +49,15 @@ def crud_login():
 
 @app_crud.route('/authorize/', methods=["GET", "POST"])
 def crud_authorize():
-    # check form inputs and create auth user
+    # check form inputs and creates user
     if request.form:
         # validation should be in HTML
         user_name = request.form.get("user_name"),
         email = request.form.get("email"),
         password1 = request.form.get("password1")
-        password2 = request.form.get("password1")
-        # model_authorize requires user_dict: user_name, email, password
-        authorize(user_name[0], email[0], password1)
-        return redirect(url_for('crud.crud_login'))
+        password2 = request.form.get("password1")          # password should be verified
+        if authorize(user_name[0], email[0], password1):    # zero index [0] used as user_name and email are type tuple
+            return redirect(url_for('crud.crud_login'))
     # show the auth user page if the above fails for some reason
     return render_template("authorize.html")
 
@@ -123,10 +130,3 @@ def search_term():
     term = req['term']
     response = make_response(jsonify(users_ilike(term)), 200)
     return response
-
-
-# this code lets Flask-Login take unauthorised users back to the login page
-@login_manager.unauthorized_handler
-def unauthorized():
-    """Redirect unauthorized users to Login page."""
-    return redirect(url_for('crud.crud_login'))
